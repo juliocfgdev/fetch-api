037006abfaa2edf81aaabe878bef7189e01e474c
Julio Guimaraes <juliocefg@gmail.com>
2020-06-29 15:01:56 -0300

    reviewed to use arrow functions

--- app.js
+++ app.js
@@ -4,48 +4,32 @@ document.getElementById('button3').addEventListener('click', getExternal);
 // Local TXT
 function getText() {
     fetch('test.txt')
-        .then(function (res) {
-            return res.text();
-        })
-        .then(function (data) {
-            document.getElementById('output').innerHTML = data
-        })
-        .catch(function (err) {
-            console.log(err);
-        })
+        .then(res => res.text())
+        .then(data => document.getElementById('output').innerHTML = data)
+        .catch(err => console.log(err))
 }
 // Local JSON
 function getJson() {
     fetch('posts.json')
-        .then(function (res) {
-            return res.json();
-        })
-        .then(function (data) {
+        .then(res => res.json())
+        .then(data => {
             let output = '';
-            data.forEach(function (post) {
+            data.forEach(post => {
                 output += `<li>${post.title}</li>
                <ul> <li>${post.body}</li></ul>`
             })
             document.getElementById('output').innerHTML = output
         })
-        .catch(function (err) {
-            console.log(err);
-        })
+        .catch(err => console.log(err))
 }
 // External API
 function getExternal() {
     fetch('https://api.github.com/users')
-        .then(function (res) {
-            return res.json();
-        })
-        .then(function (data) {
+        .then(res => res.json())
+        .then(data => {
             let output = '';
-            data.forEach(function (user) {
-                output += `<li>${user.login}</li>`
-            })
+            data.forEach(user => output += `<li>${user.login}</li>`)
             document.getElementById('output').innerHTML = output
         })
-        .catch(function (err) {
-            console.log(err);
-        })
+        .catch(err => console.log(err))
 }
